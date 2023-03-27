b5828d2ad3a59f3b830f43d359e7e88697e86f57
Xytrux <helloworldpy103@gmail.com>
2023-03-26 19:48:02 -0700

    removed the clear from compiler

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 make
-clear
+
 ./y43
--- y43.c
+++ y43.c
@@ -10,8 +10,8 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
